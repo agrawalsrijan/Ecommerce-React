bfbcfda15bb0f09faa4a54a2d0e82fef8dc060b2
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-09-16 18:34:25 +0530

    Adding Homepage component and styles for it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from "./components/HomePage"
 // initial commit
 function App() {
   return (
-    <div className="App">
- 
+    <div>
+      <HomePage/>
     </div>
   );
 }
