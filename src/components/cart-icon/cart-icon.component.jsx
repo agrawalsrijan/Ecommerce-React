15f3721cc033dfe02265c2ee8686ec5110d8c24f
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-09-19 00:24:08 +0530

    showing cartItems in cart. added cartItem component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,11 +4,11 @@ import {toggleCartHidden} from "../../redux/cart/cart.actions"
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 import "./cart-icon.styles.scss"
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({toggleCartHidden, itemCount}) => {
     return(
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
@@ -16,8 +16,16 @@ const CartIcon = ({toggleCartHidden}) => {
 const mapDispatchToProps = dispatch => {
     return {
         toggleCartHidden: () => dispatch(toggleCartHidden())
-    }
-    
+    } 
 }
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: { cartItems } }) => {
+    return {
+        itemCount: cartItems.reduce(
+            (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 
+            0
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
