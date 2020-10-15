536e36580ed82ca2258e1d791422fb7b9c71d428
Srijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-10-15 15:34:53 +0530

    updated firabse utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,6 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
 
-const config = {
-    apiKey: "AIzaSyDm2agKeXt4CLmWrbTzTPsCNDv6LB4bSiA",
-    authDomain: "crown-db-srijan.firebaseapp.com",
-    databaseURL: "https://crown-db-srijan.firebaseio.com",
-    projectId: "crown-db-srijan",
-    storageBucket: "crown-db-srijan.appspot.com",
-    messagingSenderId: "1049812363436",
-    appId: "1:1049812363436:web:8d957cdebd6a30aff19709"
-};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
